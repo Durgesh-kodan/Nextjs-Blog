9c5f8c3c7d2d9832f0729ff11a7050b5c7ec553e
Durgesh <durgeshkodan101@gmail.com>
2026-04-24 16:04:22 +0530

    small fixes

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 import { createClient, type GenericCtx } from "@convex-dev/better-auth";
 import { convex } from "@convex-dev/better-auth/plugins";
 import { components } from "./_generated/api";
@@ -5,8 +6,7 @@ import { DataModel } from "./_generated/dataModel";
 import { query } from "./_generated/server";
 import { betterAuth } from "better-auth";
 
-const siteUrl = process.env.SITE_URL!;
-
+const siteUrl = process.env.SITE_URL ?? "http://localhost:3000";
 // The component client has methods needed for integrating Convex with Better Auth,
 // as well as helper methods for general use.
 export const authComponent = createClient<DataModel>(components.betterAuth);
